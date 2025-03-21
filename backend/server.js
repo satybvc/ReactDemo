b8305896eebbf1cb4d1ee68920e49b06a7edd91e
satyandera <satyandera.soni@bvceservices.com>
2025-03-21 12:34:29 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mysql from "mysql2";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 app.use(cors()); // for allowing cross-origin requests from frontend to backend server.It is a middleware function.
 app.use(express.json()); // for parsing application/json
 
